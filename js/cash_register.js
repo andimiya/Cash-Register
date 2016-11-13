259ff7db4128a4e4381bc2c148518d4ec6226dc9
Andrea <takamiya.andrea@gmail.com>
2016-11-12 19:46:48 -1000

    Created eval function. Pressing buttons on the calculator calculates totals. Need to assign the operators to the correct button classes now.

--- js/cash_register.js
+++ js/cash_register.js
@@ -23,8 +23,13 @@ function cashRegister(){
 }
 
 
-var cashReg = cashRegister();
-console.log(cashReg.afterDeposit(10));
+
+Memory  = "0";      // initialise memory variable
+Current = "0";      //   and value of Display ("current" value)
+Operation = 0;      // Records code for eg * / etc.
+
+console.log(5*5,'25');
+
 
 var calculator = calculatorModule();
 var addingArray = [];
@@ -33,13 +38,6 @@ var addingArray = [];
 
 button2.addEventListener('click', function (){
   document.getElementById('display').innerHTML = addingArray.push(2);
-  // console.log(addingArray);
-  // var numberString = addingArray.join('');
-  // var numberTest = parseInt(numberString);
-
-  // console.log(numberTest, 'numbertest');
-  // // console.log(numberString);
-  // console.log(calculator.add(numberTest),'added');
   console.log(addingArray);
 
 });
@@ -52,9 +50,25 @@ button4.addEventListener('click', function (){
 });
 
 button1.addEventListener('click', function (){
-  var joinArray = addingArray.join('');
-  document.getElementById('display').innerHTML = addingArray.add(joinArray);
+  document.getElementById('display').innerHTML = addingArray.push('+');
 
   console.log(addingArray);
 
 });
+
+button5.addEventListener('click', function (){
+  document.getElementById('display').innerHTML = eval(addingArray.join(' '));
+});
+
+// console.log(eval(addingArray.join(' ')));
+
+
+// myArray = ["2", "+", "4", "-","1"]; // 5
+
+// eval(myArray[0] + myArray[1] + myArray[2]);
+
+// console.log(eval(myArray.join(' ')));
+
+
+
+

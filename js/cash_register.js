4f504046cb6be97a08eac7f7316602a61cb98ed1
Andrea <takamiya.andrea@gmail.com>
2016-11-13 12:17:03 -1000

    Removed the space between .join('') and set up all the button classes to match to input values and operators so the calculator works with exception of the cashregister functions. Will work on the cashregister functions now.

--- js/cash_register.js
+++ js/cash_register.js
@@ -30,8 +30,21 @@ var addingArray = [];
 
 
 
+button1.addEventListener('click', function (){
+  document.getElementById('display').innerHTML = addingArray.push(1);
+  console.log(addingArray);
+
+});
+
 button2.addEventListener('click', function (){
   document.getElementById('display').innerHTML = addingArray.push(2);
+  console.log(addingArray);
+
+});
+
+
+button3.addEventListener('click', function (){
+  document.getElementById('display').innerHTML = addingArray.push(3);
   console.log(addingArray);
 
 });
@@ -42,13 +55,85 @@ button4.addEventListener('click', function (){
 
 });
 
-button1.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push('+');
+
+button5.addEventListener('click', function (){
+  document.getElementById('display').innerHTML = addingArray.push(5)
   console.log(addingArray);
 
 });
 
-button5.addEventListener('click', function (){
-  var totalForNow = eval(addingArray.join(' '));
+
+button6.addEventListener('click', function (){
+  document.getElementById('display').innerHTML = addingArray.push(6);
+  console.log(addingArray);
+
+});
+
+
+button7.addEventListener('click', function (){
+  document.getElementById('display').innerHTML = addingArray.push(7);
+  console.log(addingArray);
+
+});
+
+button8.addEventListener('click', function (){
+  document.getElementById('display').innerHTML = addingArray.push(8)
+  console.log(addingArray);
+
+});
+
+
+button9.addEventListener('click', function (){
+  document.getElementById('display').innerHTML = addingArray.push(9)
+  console.log(addingArray);
+
+});
+
+button0.addEventListener('click', function (){
+  document.getElementById('display').innerHTML = addingArray.push(0);
+  console.log(addingArray);
+
+});
+
+
+button00.addEventListener('click', function (){
+  document.getElementById('display').innerHTML = addingArray.push(00);
+  console.log(addingArray);
+
+});
+
+buttonDecimal.addEventListener('click', function (){
+  document.getElementById('display').innerHTML = addingArray.push(".")
+  console.log(addingArray);
+
+});
+
+
+buttonMultiply.addEventListener('click', function (){
+  document.getElementById('display').innerHTML = addingArray.push("*")
+  console.log(addingArray);
+
+});
+
+buttonDivide.addEventListener('click', function (){
+  document.getElementById('display').innerHTML = addingArray.push('/');
+  console.log(addingArray);
+
+});
+buttonAdd.addEventListener('click', function (){
+  document.getElementById('display').innerHTML = addingArray.push("+")
+  console.log(addingArray);
+
+});
+
+buttonSubtract.addEventListener('click', function (){
+  document.getElementById('display').innerHTML = addingArray.push("-")
+  console.log(addingArray);
+
+});
+
+buttonEqual.addEventListener('click', function (){
+  var totalForNow = eval(addingArray.join(''));
   document.getElementById('display').innerHTML = totalForNow;
 });
+

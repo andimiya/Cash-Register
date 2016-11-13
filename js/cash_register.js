427d2cb2a1bc60674073112ec59f0eb0e6f6771c
Andrea <takamiya.andrea@gmail.com>
2016-11-12 16:37:06 -1000

    Removed some commented out stuff that we didn't need

--- js/cash_register.js
+++ js/cash_register.js
@@ -5,16 +5,8 @@
 var calculator = calculatorModule();
 console.log(calculator.load(5));
 
-// display.innerHTML = calculator.load(5);
-
-function testing (){
-  console.log('test');
-
-}
 
 document.addEventListener('click', function (){
   document.getElementById('display').innerHTML = calculator.load(5);
 });
 
-// var displayTest = document.getElementById('display');
-// displayTest.addEventListener('click', testing);

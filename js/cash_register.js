b963aa0e5094fd40d90560137b01dbe7148dee78
Naoto Yamaguchi <naotoy@hawaii.edu>
2016-11-12 16:52:33 -1000

    made a basic outline of cash register

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,12 +1,35 @@
+function cashRegister(){
+  var balance = 0;
 
+  function deposit(x){
+    balance += x;
+    return balance;
+  }
 
+  function withdraw(x){
+    balance -= x;
+    return balance;
+  }
+
+  function clear(){
+    balance = 0;
+    return balance;
+  }
+
+  return{
+    afterDeposit: deposit,
+    afterWithdraw: withdraw
+  };
+}
 
 
 var calculator = calculatorModule();
 console.log(calculator.load(5));
+// var Asdf = document.getElementById('display');
 
+// Asdf.addEventListener('click', function (){
+//   Asdf.getElementById('display').innerHTML = calculator.load(5);
+// });
 
-document.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = calculator.load(5);
-});
-
+var cashReg = cashRegister();
+console.log(cashReg.afterDeposit(10));

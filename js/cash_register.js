70b65d85440b18db266f3f7f43459909a2a25d6e
Andrea <takamiya.andrea@gmail.com>
2016-11-13 16:17:19 -1000

    Now the display will print out the numbers that were pressed in the calculator along with the operator symbol that was pressed and shows the calculated result

--- js/cash_register.js
+++ js/cash_register.js
@@ -36,109 +36,102 @@ clear.addEventListener('click', function (){
 });
 
 button1.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push(1);
+  addingArray.push(1);
+  document.getElementById('display').innerHTML = addingArray.join('');
   console.log(addingArray);
-
 });
 
 button2.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push(2);
+  addingArray.push(2);
+  document.getElementById('display').innerHTML = addingArray.join('');
   console.log(addingArray);
-
 });
 
-
 button3.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push(3);
+  addingArray.push(3);
+  document.getElementById('display').innerHTML = addingArray.join('');
   console.log(addingArray);
-
 });
 
 button4.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push(4)
+  addingArray.push(4);
+  document.getElementById('display').innerHTML = addingArray.join('');
   console.log(addingArray);
-
 });
-
 
 button5.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push(5)
+  addingArray.push(5);
+  document.getElementById('display').innerHTML = addingArray.join('');
   console.log(addingArray);
-
 });
-
 
 button6.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push(6);
+  addingArray.push(6);
+  document.getElementById('display').innerHTML = addingArray.join('');
   console.log(addingArray);
-
 });
 
-
 button7.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push(7);
+  addingArray.push(7);
+  document.getElementById('display').innerHTML = addingArray.join('');
   console.log(addingArray);
-
 });
 
 button8.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push(8)
+  addingArray.push(8);
+  document.getElementById('display').innerHTML = addingArray.join('');
   console.log(addingArray);
-
 });
 
-
 button9.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push(9)
+  addingArray.push(9);
+  document.getElementById('display').innerHTML = addingArray.join('');
   console.log(addingArray);
-
 });
 
 button0.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push(0);
+  addingArray.push(0);
+  document.getElementById('display').innerHTML = addingArray.join('');
   console.log(addingArray);
-
 });
 
-
 button00.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push(00);
+  addingArray.push(00);
+  document.getElementById('display').innerHTML = addingArray.join('');
   console.log(addingArray);
-
 });
 
 buttonDecimal.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push(".")
+  addingArray.push(".");
+  document.getElementById('display').innerHTML = addingArray.join('');
   console.log(addingArray);
-
 });
 
-
 buttonMultiply.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push("*")
+  addingArray.push("*");
+  document.getElementById('display').innerHTML = addingArray.join('');
   console.log(addingArray);
-
 });
 
 buttonDivide.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push('/');
+  addingArray.push("/");
+  document.getElementById('display').innerHTML = addingArray.join('');
   console.log(addingArray);
-
 });
-buttonAdd.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push("+")
-  console.log(addingArray);
 
+buttonAdd.addEventListener('click', function (){
+  addingArray.push("+");
+  document.getElementById('display').innerHTML = addingArray.join('');
+  console.log(addingArray);
 });
 
 buttonSubtract.addEventListener('click', function (){
-  document.getElementById('display').innerHTML = addingArray.push("-")
+  addingArray.push("-");
+  document.getElementById('display').innerHTML = addingArray.join('');
   console.log(addingArray);
-
 });
 
 buttonEqual.addEventListener('click', function (){
   var totalForNow = eval(addingArray.join(''));
   document.getElementById('display').innerHTML = totalForNow;
 });
-
